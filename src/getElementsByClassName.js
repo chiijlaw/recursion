79d2d323b86a74051109b37db82f44ef9d4f661d
Chiijlaw <chiijlaw@berkeley.edu>
2018-07-07 22:51:03 -0700

    included all of the html document in the for loop

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,30 +4,23 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  var output=[];
-  if (document.body.classList.contains(className)) {
-  	output.push(document.body);
-  }
-  for (var node of document.body.childNodes) {
-  	if ( node.classList && node.classList.contains(className)){
-  		output.push(node);
-  	}
-  }
-  //console.log(elementClasses);
-  //var getByClassName = function (list,target) {
-  	//var comparison;
-  	//if (elementClasses.length === 0){
-  	//	return output;
-  	//}
-  	//comparison = list.pop();
-  	//if (comparison == target){
-  	//	output.push(comparison);
-  	//}
-  	//getByClassName(list,target);
-  //};
-  //getByClassName(elementClasses,className);
-  //console.log(output);
-  return output;
+var getElementsByClassName = function(className) {
+	var output=[];
+	//if (document.body.classList.contains(className)) {
+	//	output.push(document.body);
+	//}
+	for (var node of document.documentElement.childNodes) {
+		if ( node.classList && node.classList.contains(className)){
+			output.push(node);
+		}
+		if (node.hasChildNodes()){
+			//for (var i = node.children.length-1;i>=0;i--) {
+			for (var node2 of node.childNodes) {
+				if ( node2.classList && node2.classList.contains(className)){
+					output.push(node2);
+				}
+			}
+		}
+	}
+	return output;
 };
