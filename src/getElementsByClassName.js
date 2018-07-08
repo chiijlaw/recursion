502b99114f1735700104a1f7a35e9ad1402f733a
Chiijlaw <chiijlaw@berkeley.edu>
2018-07-07 21:27:57 -0700

    getElementsByClassName searches through childnodes' classlist

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,13 +6,28 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className
 ) {
-  var output;
-  var elementClasses = document.body.classList;
-  console.log(elementClasses);
-  var getByClassName = function (list,target) {
-  	if (elementClasses.length === 0){
-  		return output;
+  var output=[];
+  if (document.body.classList.contains(className)) {
+  	output.push(document.body);
+  }
+  for (var node of document.body.childNodes) {
+  	if ( node.classList && node.classList.contains(className)){
+  		output.push(node);
   	}
-  };
-  getByClassName(elementClasses,className);
+  }
+  //console.log(elementClasses);
+  //var getByClassName = function (list,target) {
+  	//var comparison;
+  	//if (elementClasses.length === 0){
+  	//	return output;
+  	//}
+  	//comparison = list.pop();
+  	//if (comparison == target){
+  	//	output.push(comparison);
+  	//}
+  	//getByClassName(list,target);
+  //};
+  //getByClassName(elementClasses,className);
+  //console.log(output);
+  return output;
 };
