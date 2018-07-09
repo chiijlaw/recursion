8378cff0dec1d374c1d531370e508d53c170eeb0
Chiijlaw <chiijlaw@berkeley.edu>
2018-07-08 18:14:59 -0700

    Adds compatibility with an empty object

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -9,31 +9,37 @@ var stringifyJSON = function(obj) {
 		var result;
 		if (typeof element == "boolean" || typeof element == "number" ){
 		result = element.toString();
-		} else if (element == null) {
-			result = "null";
-		} else if (element == undefined) {
+		}  else if (element == undefined) {
 			result = "undefined";
 		} else if (typeof element == "string") {
 			result = '\"'+element+'\"';
 		}
 		return result;
 	};
-	if (!Array.isArray(obj)) {
+	if (!Array.isArray(obj) && typeof obj != "object") {
 		output = stringFormat(obj);
-	}
-	if (Array.isArray(obj)) {
+	} else if (Array.isArray(obj)) {
 		output = "[]";
 		var inside = [];
 		for (var i of obj){
 			if (!Array.isArray(i)){
 				inside.push(stringFormat(i));
-			}
-			 if (Array.isArray(i)){
+			} else if (Array.isArray(i)){
 				inside.push(stringifyJSON(i));
 			}
 		}
 		if (!!inside){
 			output = '['+inside+']';
+		}
+	} else if (typeof obj == "object" && !Array.isArray(obj)) {
+		if (obj == null) {
+			output = "null";
+		} else {
+			output = "{}";
+			var inside = {};
+			if (Object.entries(inside).length > 0){
+				output = '{'+inside+'}';
+			}
 		}
 	}
 	return output;
